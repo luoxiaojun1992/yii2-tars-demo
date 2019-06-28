363fd8e21f5865efb2b3f0576e8da3028c3dd91f
luoxiaojun <luoxiaojun@dodoca.com>
2019-06-28 22:04:45 +0800

    fix request context bug

--- src/controllers/TarsController.php
+++ src/controllers/TarsController.php
@@ -12,7 +12,7 @@ class TarsController extends Controller
     public function actionHttp()
     {
         Yii::info('test yii2 tars log');
-        return '接入Yii2 Router 成功';
+        return '接入Yii2 Router 成功,入参:' . json_encode(\Yii::$app->request->get());
     }
 
     public function actionTars()
